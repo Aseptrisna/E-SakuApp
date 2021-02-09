8d4eec4995ee2b7161ecf668158f869061a9e079
AsepTrisna <Aseptrisna1995@gmail.com>
2021-02-09 11:24:39 +0700

    template

---
+++ app/src/main/java/com/omkabel/e_saku/Features/Menu_Profile.java
@@ -0,0 +1,16 @@
+package com.omkabel.e_saku.Features;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.omkabel.e_saku.R;
+
+public class Menu_Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu__profile);
+    }
+}
---
+++ app/src/main/java/com/omkabel/e_saku/Features/Menu_SplashScreen.java
@@ -0,0 +1,13 @@
+package com.omkabel.e_saku.Features;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.omkabel.e_saku.R;
+public class Menu_SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu__splash_screen);
+    }
+}
